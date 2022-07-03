e2531ea4ebb34f7dc0058f3bd791f1717df80b7f
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-03 11:26:54 +0300

    Ok

---
+++ script/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let admin;
+const name = "Джон";
+admin = name; 
+alert(admin);
+
+const ourPlanetName = "Земля";
+const currentUserName = "Сергей";
+
+
+const result = 10 + '10';
+console.log(result, typeof result);
